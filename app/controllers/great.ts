3534c497bb09168a1a444e111913466eb084b6c6
Big Mogician <sunaoran@youzan.com>
2018-12-19 00:28:22 +0800

    update demo

--- app/controllers/great.ts
+++ app/controllers/great.ts
@@ -1,5 +1,5 @@
 import { Controller, Index, RenderResult, Render } from "astroboy.ts";
-import { TestService } from "demo2.astroboy.ts";
+import { TestService, MixinService } from "demo2.astroboy.ts";
 import X0c1Service from "./../services/x0c1";
 
 @Controller("great")
@@ -7,17 +7,20 @@ class GreatController {
 
   constructor(
     private render: Render,
+    private mixin: MixinService,
     private x0c1: X0c1Service,
     private test: TestService) { }
 
   @Index([""])
   public imdexHtml() {
     this.render.setView("viewNum", this.test.showValue());
+    this.render.setView("mixinNum", this.mixin.stamp);
     return new RenderResult({
-      // path: "great.njk",
-      // engine: "nunjunks"
-      path: "great2.ejs",
-      engine: "ejs"
+      // root: "/sbx",
+      path: "great.njk",
+      engine: "nunjunks"
+      // path: "great2.ejs",
+      // engine: "ejs"
     });
   }
 
